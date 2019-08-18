4918a4769d0d5afaf9264a72ba5b3bd04a9a2e21
Tomek Vivobook <tomasz.k.gac@gmail.com>
2019-08-18 13:55:24 +0200

    opis we wrapper_traits

--- prim/vtable/wrapper_traits.hpp
+++ prim/vtable/wrapper_traits.hpp
@@ -5,6 +5,9 @@ namespace prim{
 
 template< typename T >
 struct wrapper_traits{
+
+  // specialization for wrapper types requires that get<0>
+  // returns the wrapper object itself so that it can be stored in value
   template< size_t index, typename U >
   static decltype(auto) get( U&& u ){
     static_assert( index == 0, "Cannot use enumerated placeholders for non-wrapper types" );
